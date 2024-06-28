7bf5287f955b4a697227fcaaef780b20ffcc86b2
Michael-Andryeer <andryeerm@gmail.com>
2024-06-27 22:07:51 -0300

    Conexão com banco de dados MongoDB com repository e service

--- src/main/java/com/example/PostWave/resources/UserResource.java
+++ src/main/java/com/example/PostWave/resources/UserResource.java
@@ -1,10 +1,11 @@
 package com.example.PostWave.resources;
 
 import com.example.PostWave.domain.User;
+import com.example.PostWave.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -15,12 +16,12 @@ import java.util.List;
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
     public ResponseEntity<List<User>> findAll(){
-        User vitoria = new User("1","vitoria carol","vitoria@gmail.com");
-        User michael = new User("2","michael andryeer","michael@gmail.com");
-        List<User> list = new ArrayList<>();
-        list.addAll(Arrays.asList(vitoria,michael));
+        List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
