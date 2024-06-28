5d0ac06a99a9f5cee677a35d314b297f240bc099
Michael-Andryeer <andryeerm@gmail.com>
2024-06-28 00:11:47 -0300

    Obtendo um usúario pelo Id.

--- src/main/java/com/example/PostWave/services/UserService.java
+++ src/main/java/com/example/PostWave/services/UserService.java
@@ -2,10 +2,12 @@ package com.example.PostWave.services;
 
 import com.example.PostWave.domain.User;
 import com.example.PostWave.repository.UserRepository;
+import com.example.PostWave.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,12 @@ public class UserService {
 
     public List<User> findAll(){
         return repository.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> user = repository.findById(id);
+        if(user.isEmpty())
+            throw new ObjectNotFoundException("Objeto não encontrado");
+        return user.orElse(null);
     }
 }
